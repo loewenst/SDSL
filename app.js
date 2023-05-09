78fa9dc070e57452d34746c1cff8ffab700b3d4e
Daniel Loewenstein <loewenst@gmail.com>
2023-05-09 10:07:27 -0400

    got the text color to reset for new questions and the scorebar to change

--- app.js
+++ app.js
@@ -25,8 +25,30 @@ const song2 = {
   choices: ['Owl City', 'Taylor Swift', 'Backstreet Boys', 'A Great Big World'],
   artist: 'Owl City'
 }
+const song3 = {
+  lyrics: 'Oh, where do we begin? </br> The rubble, or our sins?',
+  choices: ['Creed', 'Bastille', 'Imagine Dragons', 'Panic! at the Disco'],
+  artist: 'Bastille'
+}
+const song4 = {
+  lyrics:
+    "I can't remember all the times I tried to tell myself </br> to hold on to these moments as they pass",
+  choices: [
+    'Counting Crows',
+    'The Wallflowers',
+    'Jack’s Mannequin',
+    'Dave Matthews Band'
+  ],
+  artist: 'Counting Crows'
+}
+const song5 = {
+  lyrics:
+    "We got the bubble-headed bleach-blonde who comes on at five </br> She can tell you about the plane crash with a gleam in her eye </br> It's interesting when people die",
+  choices: ['Don Henley', 'U2', 'Panic! at the Disco', 'Green Day'],
+  artist: 'Don Henley'
+}
 
-const songObjects = [song1]
+const songObjects = [song1, song2, song3, song4, song5]
 const readyMesssages = ['Ready', 'Set', 'Go']
 
 //********************* State Variables *********************
@@ -36,6 +58,7 @@ let answer = false
 let hints
 let hardHints = false
 let hardQuestions = false
+let currentSong
 
 //********************* Cached Elements *********************
 const readyMessageEl = document.getElementById('ready')
@@ -45,11 +68,61 @@ const choice2 = document.getElementById('answer2')
 const choice3 = document.getElementById('answer3')
 const choice4 = document.getElementById('answer4')
 const scoreBarEls = [...document.getElementsByClassName('score')]
+const answerEls = [...document.getElementsByClassName('answer')]
 
 //********************* Functions *********************
 
+const nextQuestion = () => {
+  let currentSongId = songObjects.indexOf(currentSong)
+  if (currentSongId === 0) {
+    songObjects.shift()
+  } else {
+    songObjects.splice(currentSongId, 1)
+  }
+  setTimeout(() => {
+    answerEls.forEach((answerEl) => {
+      answerEl.style.backgroundColor = 'black'
+      answerEl.style.color = 'white'
+    })
+    render()
+  }, 1500)
+}
+
+const choose = (evt) => {
+  console.log(evt.target)
+  const answerIdx = answerEls.indexOf(evt.target)
+  console.log(answerIdx)
+  if (answerIdx === -1) return
+  evt.target.style.backgroundColor = 'gray'
+  evt.target.style.color = 'black'
+  //audio.play()
+  console.log(evt.target.innerText)
+  timer = setTimeout(() => {
+    let currentScoreBox = scoreArray.indexOf(0)
+    if (evt.target.innerText === currentSong.artist) {
+      evt.target.style.backgroundColor = 'green'
+      scoreArray[currentScoreBox] = 1
+    } else {
+      evt.target.style.backgroundColor = 'yellow'
+      answerEls.forEach((answerEl) => {
+        if (answerEl.innerText === currentSong.artist) {
+          answerEl.style.backgroundColor = 'green'
+        }
+      })
+      scoreArray[currentScoreBox] = -1
+    }
+    if (scoreArray === [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) {
+      win()
+    } else if (currentScoreBox === -1) {
+      lose()
+    } else {
+      nextQuestion()
+    }
+  }, 1500)
+}
+
 const populateQuestion = () => {
-  let currentSong = songObjects[Math.floor(Math.random() * songObjects.length)]
+  currentSong = songObjects[Math.floor(Math.random() * songObjects.length)]
   lyric.innerHTML = currentSong.lyrics
   choice1.innerText = currentSong.choices[0]
   choice2.innerText = currentSong.choices[1]
@@ -59,6 +132,7 @@ const populateQuestion = () => {
 
 const renderQuestion = () => {
   count = 0
+  readyMessageEl.innerText = readyMesssages[count]
   readyMessageEl.style.visibility = 'visible'
   const timer = setInterval(() => {
     count++
@@ -69,7 +143,7 @@ const renderQuestion = () => {
       readyMessageEl.style.visibility = 'hidden'
     }
   }, 750)
-  setTimeout(populateQuestion, 2000)
+  setTimeout(populateQuestion, 2250)
 }
 
 //since I made this an array, not sure if the style change will work
@@ -99,3 +173,5 @@ const init = () => {
 init()
 
 //********************* Event Handlers *********************
+
+document.getElementById('answerspace').addEventListener('click', choose)
