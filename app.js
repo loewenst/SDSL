574c2fe63b2fbcda6da2aa9dee52a23606b56cbc
Daniel Loewenstein <loewenst@gmail.com>
2023-05-12 12:13:51 -0400

    added audio clips to play for each song after reveal

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const song = {
   explainer: {
     heading: "",
     text: ""
-  }
+  },
+  clip: ""
 }
 */
 
@@ -44,7 +45,8 @@ const song1 = {
   explainer: {
     heading: 'Song: Name | Artist: The Goo Goo Dolls | Album: A Boy Named Goo',
     text: 'This line always struck home because of the way it acknowledges that the things that are important to us in our own little lives probably seem petty and insignificant if we just draw back the lens and think beyond ourselves - but it doesn’t make the point in a moralizing way. It’s not saying, "Stop caring about the things that don’t matter,” it’s saying, hey, the fact that the things you care about don’t matter to the wider world - that really sucks, doesn’t it? And that’s so validating to hear.'
-  }
+  },
+  clip: 'audio/Name Clip.wav'
 }
 const song2 = {
   lyrics: 'My dreams are bursting at the seams',
@@ -61,7 +63,8 @@ const song2 = {
   explainer: {
     heading: 'Song: Fireflies | Artist: Owl City | Album: Ocean Eyes',
     text: 'It’s basically a signature for Owl City (a.k.a. Adam Young) to write songs that are optimistic, unjaded, and full of imagination and childlike wonder (a song about living underwater? He’s got it. Blasting off into space? He’s got it. A song about awkwardness talking to girls that still feels upbeat and innocent? He’s got it.). This line feels like a peek into the mind behind that signature - a mind flooded with so many hopes and ideas and excitement about the possibilities of the future that it feels like it could burst. To me, it lands as a reminder to tamp down the cynicism, and aspire for some of that wonder and excitement about the open future.'
-  }
+  },
+  clip: 'audio/Fireflies Clip.wav'
 }
 
 const song3 = {
@@ -80,7 +83,8 @@ const song3 = {
   explainer: {
     heading: 'Song: Pompeii | Artist: Bastille | Album: Bad Blood',
     text: `Tragedy is a part of life. It happens to everyone at some point, even if not usually on the scale of Pompeii getting obliterated by a volcano. And sometimes the tragedy just happens, but sometimes it feels like we bring it on ourselves, because of our bad decisions, karma, or whatever it is. And when you’re picking up the pieces after that kind of tragedy, boy does this question pack a punch. Do I start with figuring out the logistics of my life that have to change? Find a new place, get a new job, get back in the dating scene? Or do I start with my attitude, my selfishness, my fear of commitment, or whatever landed me there? What’s the bigger fire, the outcome or the cause? And what’s the bigger tragedy? Where do we begin?`
-  }
+  },
+  clip: 'audio/Pompeii Clip.wav'
 }
 const song4 = {
   lyrics:
@@ -105,7 +109,8 @@ const song4 = {
     heading:
       'Song: A Long December | Artist: Counting Crows | Album: Recovering the Satellites',
     text: 'This line captures a crazy thing about being human: just because you were there doesn’t mean you experienced it. Even in the very best or most important moments, if you’re not present, if you’re consumed with something else, dwelling on the past or worrying about the future, then you miss it. But even if you know it, that doesn’t make it easy. You can try and try to be present and fail, and feel like the best things are passing you by but you can’t get out of your head long enough to feel them. We just gotta keep trying. (And meditate!)'
-  }
+  },
+  clip: 'audio/Long December Clip.wav'
 }
 const song5 = {
   lyrics:
@@ -127,7 +132,8 @@ const song5 = {
       'Song: Dirty Laundry | Artist: Don Henley | Album: I Can’t Stand Still',
     text: `This song, which is a cutting indictment of the news media, feels like it could’ve been written yesterday as easily as 1982. Henley calls out the industry for the fact that it has a built-in incentive to report tragic, scandalous or otherwise sensational news, and thus warps the ways news personnel experience all those things. The song implicitly calls for us to figure out a way for the news media to be able to inform while also being able to stay human.
     `
-  }
+  },
+  clip: 'audio/Dirty Laundry Clip.wav'
 }
 const song6 = {
   lyrics:
@@ -145,7 +151,8 @@ const song6 = {
   explainer: {
     heading: 'Song: Dig | Artist: Incubus | Album: Light Grenades',
     text: `We’re never the same person all the time with everyone in every situation. And sometimes who we are at a given point in time can be a little ugly. Or a lot. But if we’re lucky, we have people in our lives that can see through who we happen to be at our low points, to who we were before, or who we are at our best, or who we could be. And if we’re really lucky, they can help us find our way back or our way out. This song celebrates having that kind of relationship.`
-  }
+  },
+  clip: 'audio/Dig Clip.wav'
 }
 
 const song7 = {
@@ -162,7 +169,8 @@ const song7 = {
   explainer: {
     heading: 'Song: I Lived | Artist: OneRepublic | Album: Native',
     text: `This clever play on words, paradoxically wishing for us to have more after we spend everything, is a nod to two pitfalls in life: one, letting our time go by instead of <i>spending</i> it - using it, and choosing how we use it - and two, regretting our experiences because they don’t fit the narrative we want ourselves to have, instead of appreciating them as part of ourselves, the threads that add up to the tapestries of our lives. Here’s to hoping we navigate those pitfalls, we grab life by the horns, and we love the ride for what it is.`
-  }
+  },
+  clip: 'audio/I Lived Clip.wav'
 }
 
 const song8 = {
@@ -186,7 +194,8 @@ const song8 = {
     heading:
       'Song: Jack and Diane | Artist: John Mellencamp | Album: American Fool',
     text: `I must have heard this song fifty times before I really paid attention to these words - the wistful, folksy tone of the music doesn’t lead you to expect lyrics that are so heavy. The song is about two high school sweethearts growing up, and the fun and optimism and ambition of being a teenager that disappears as you get older. We might all try “holding on to sixteen as long as you can," but at the end of the day, life gets less shiny, the colors get a little more dull, but it still goes on, and we have to come to terms with that. (Which makes it wonderfully ironic that Jessica Simpson sampled this track for a bubblegum pop love song.)`
-  }
+  },
+  clip: 'audio/Jack and Diane Clip.wav'
 }
 
 const song9 = {
@@ -208,7 +217,8 @@ const song9 = {
     text: `
     A huge part of being a good friend or partner is listening, supporting, encouraging, and bringing the fun and good vibes to the relationship. But that's one sided, even if it is the "nicer," giving side. In the deepest relationships, both sides have to be generous enough to be there when needed, <i>and</i> to be vulnerable enough to need. And for a lot of us, that's really hard, because having needs means looking inside, knowing where we’re incomplete, and letting someone else see that too. You just can't do that if you're in denial about those parts of yourself, if you can’t be okay with the fact your shortcomings are a real part of you. That’s what these lyrics are about - one partner telling the other, accept yourself enough to trust me to see inside and still love you. That’s how you can love me. Just saying, that girl’s a catch.
     `
-  }
+  },
+  clip: 'audio/Whatever It Takes Clip.wav'
 }
 
 const song10 = {
@@ -228,7 +238,8 @@ const song10 = {
   explainer: {
     heading: 'Song: Mad Season | Artist: Matchbox 20 | Album: Mad Season',
     text: `Oof. It’s classic Matchbox 20 to sing about a dysfunctional relationship, and this line stands out to me because of the casual, nonchalant yet subtly seething tone of the singer’s observation that hey, I thought this relationship was really deep, I thought this whole time we were together you noticed me, learned about me, cared about who I was. I relied on that, trusted that. Um, guess not? Part of what I love about these lyrics is the way they’re not dramatic. There’s something almost funny about them. And there’s something dignified about them too, how he’s not falling to pieces, this horrible realization hits him yet he’s passive-aggressively playing the detached observer musing about it. So much personality in this, wow.`
-  }
+  },
+  clip: 'audio/Mad Season Clip.wav'
 }
 
 const songObjectsHardCode = [
@@ -249,13 +260,12 @@ const rightAudio = new Audio('audio/Correct Answer.wav')
 const wrongAudio = new Audio('audio/Wrong Answer.wav')
 
 //********************* State Variables *********************
-let score
+let currentSongClip
 let scoreArray = []
 let answer = false
 let hintCount
 let hintbar = false
 let hardHints = false
-let hardQuestions = false
 let currentSong
 let hint1
 let hint2
@@ -425,8 +435,6 @@ const win = () => {
   commentEl.style.visibility = 'hidden'
   bounce(resetEl)
   bounce(difficultyEl)
-
-  //play win music
 }
 
 //POPUP
@@ -440,14 +448,13 @@ const lose = () => {
   nextEl.style.visibility = 'hidden'
   commentEl.style.visibility = 'hidden'
   bounce(resetEl)
-
-  //play lose music
 }
 
 //POPUP
 //This is code for a multi-functional button that will change
 //depending on which popup is displaying
 const next = () => {
+  currentSongClip.pause()
   renderScoreBar()
   hidePopUp()
   if (popupSetting === 'difficulty') {
@@ -485,6 +492,8 @@ const displayExplainer = () => {
   nextEl.style.visibility = 'visible'
   nextEl.innerHTML = 'Next'
   commentEl.style.visibility = 'hidden'
+  currentSongClip = new Audio(currentSong.clip)
+  currentSongClip.play()
 }
 
 const choose = (evt) => {
